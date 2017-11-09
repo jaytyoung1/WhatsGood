df60ba1316fcbbfe4ed47cae8d7b4481764eaa86
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2017-11-09 13:56:08 -0500

    Send user to restaurant's homepage when clicking on image.

--- app/src/main/java/com/example/android/whatsgood/Restaurant.java
+++ app/src/main/java/com/example/android/whatsgood/Restaurant.java
@@ -48,6 +48,11 @@ public class Restaurant
     public String getName() { return mName; }
 
     /**
+     * Get the link of the restaurant
+     */
+    public String getLink() { return mLink; }
+
+    /**
      * Get the image
      */
     public int getImageResourceId()
