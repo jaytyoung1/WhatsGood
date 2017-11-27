c1c0c7c7917abadb01892d5ddff5868c6029ee23
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2017-11-27 00:30:04 -0500

    Begin work on content provider

--- app/src/main/java/com/example/android/whatsgood/data/WhatsGoodContract.java
+++ app/src/main/java/com/example/android/whatsgood/data/WhatsGoodContract.java
@@ -4,6 +4,7 @@ package com.example.android.whatsgood.data;
  * Created by jyoun on 11/22/2017.
  */
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -18,12 +19,43 @@ public final class WhatsGoodContract
     }
 
     /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.android.whatsgood";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's) (PATH_table_name)
+     * For instance, content://com.example.android.whatsgood/restaurants/ is a valid path for
+     * looking at whatsgood data. content://com.example.android.whatsgood/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_RESTAURANTS = "restaurants";
+
+    /**
      * Inner class that defines constant values for the restaurants database table.
      * Each entry in the table represents a single restaurant.
      */
     public static final class RestaurantEntry implements BaseColumns
     {
+        /**
+         * The content URI to access the restaurant data in the provider
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_RESTAURANTS);
+
+        /**
+         * Name of database table for restaurants
+         */
         public final static String TABLE_NAME = "restaurants";
+
         public final static String _ID = BaseColumns._ID;
         public final static String COLUMN_RESTAURANT_NAME = "name";
         public final static String COLUMN_RESTAURANT_LINK = "link";
---
+++ app/src/main/java/com/example/android/whatsgood/data/WhatsGoodProvider.java
@@ -0,0 +1,79 @@
+package com.example.android.whatsgood.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+/**
+ * {@link ContentProvider} for Whats Good app.
+ */
+public class WhatsGoodProvider extends ContentProvider
+{
+
+    /**
+     * Tag for the log messages
+     */
+    public static final String LOG_TAG = WhatsGoodProvider.class.getSimpleName();
+
+    /**
+     * Database helper object
+     */
+    private WhatsGoodDbHelper mDbHelper;
+
+    /**
+     * Initialize the provider and the database helper object.
+     */
+    @Override
+    public boolean onCreate()
+    {
+        mDbHelper = new WhatsGoodDbHelper(getContext());
+        return true;
+    }
+
+    /**
+     * Perform the query for the given URI. Use the given projection, selection, selection arguments, and sort order.
+     */
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder)
+    {
+        return null;
+    }
+
+    /**
+     * Insert new data into the provider with the given ContentValues.
+     */
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues)
+    {
+        return null;
+    }
+
+    /**
+     * Updates the data at the given selection and selection arguments, with the new ContentValues.
+     */
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs)
+    {
+        return 0;
+    }
+
+    /**
+     * Delete the data at the given selection and selection arguments.
+     */
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs)
+    {
+        return 0;
+    }
+
+    /**
+     * Returns the MIME type of data for the content URI.
+     */
+    @Override
+    public String getType(Uri uri)
+    {
+        return null;
+    }
+}
