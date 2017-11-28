6ceefc7baff3d3c6a24fbf047536dbadf9075297
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2017-11-28 13:01:34 -0500

    Comment out the tab adapter.

--- app/src/main/java/com/example/android/whatsgood/TabAdapter.java
+++ app/src/main/java/com/example/android/whatsgood/TabAdapter.java
@@ -1,55 +1,55 @@
-package com.example.android.whatsgood;
-
-import android.content.Context;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-
-/**
- * Created by jyoun on 11/9/2017.
- */
-
-public class TabAdapter extends FragmentPagerAdapter
-{
-    /**
-     * Context of the app
-     */
-    private Context mContext;
-
-    /**
-     * Create a new {@link TabAdapter} object
-     *
-     * @param context   is the context of the app
-     * @param fm        is the fragment manager that will keep each fragment's state in the adapter
-     *                  across swipes
-     */
-    public TabAdapter(Context context, FragmentManager fm)
-    {
-        super(fm);
-        mContext = context;
-    }
-
-    /**
-     * Return the {@link Fragment} that should be displayed for the given page number
-     */
-    @Override
-    public Fragment getItem(int position)
-    {
-        if (position == 0)
-            return new ListsFragment();
-        else
-            return new MapFragment();
-    }
-
-    /**
-     * Return the total number of pages
-     */
-    @Override
-    public int getCount() { return 2; }
-
-    @Override
-    public CharSequence getPageTitle(int position)
-    {
-       return "";
-    }
-}
+//package com.example.android.whatsgood;
+//
+//import android.content.Context;
+//import android.support.v4.app.Fragment;
+//import android.support.v4.app.FragmentManager;
+//import android.support.v4.app.FragmentPagerAdapter;
+//
+///**
+// * Created by jyoun on 11/9/2017.
+// */
+//
+//public class TabAdapter extends FragmentPagerAdapter
+//{
+//    /**
+//     * Context of the app
+//     */
+//    private Context mContext;
+//
+//    /**
+//     * Create a new {@link TabAdapter} object
+//     *
+//     * @param context   is the context of the app
+//     * @param fm        is the fragment manager that will keep each fragment's state in the adapter
+//     *                  across swipes
+//     */
+//    public TabAdapter(Context context, FragmentManager fm)
+//    {
+//        super(fm);
+//        mContext = context;
+//    }
+//
+//    /**
+//     * Return the {@link Fragment} that should be displayed for the given page number
+//     */
+//    @Override
+//    public Fragment getItem(int position)
+//    {
+//        if (position == 0)
+//            return new ListsFragment();
+//        else
+//            return new MapFragment();
+//    }
+//
+//    /**
+//     * Return the total number of pages
+//     */
+//    @Override
+//    public int getCount() { return 2; }
+//
+//    @Override
+//    public CharSequence getPageTitle(int position)
+//    {
+//       return "";
+//    }
+//}
