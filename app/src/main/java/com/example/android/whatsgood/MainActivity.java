36e64f97a8ff513edea3799aacb8d010c7346ef9
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2017-11-30 11:28:07 -0500

    Comment out ViewPager and TabAdapter in MainActivity. Also, get the restaurants by calling the AsyncTask.

--- app/src/main/java/com/example/android/whatsgood/MainActivity.java
+++ app/src/main/java/com/example/android/whatsgood/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.whatsgood;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,21 +22,32 @@ import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.example.android.whatsgood.data.GetRestaurantsAsyncTask;
+import com.example.android.whatsgood.data.WhatsGoodContract;
+import com.example.android.whatsgood.data.WhatsGoodDbHelper;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
 import static java.security.AccessController.getContext;
 
-public class MainActivity extends AppCompatActivity
-//        implements OnMapReadyCallback,
-//        GoogleApiClient.ConnectionCallbacks,
-//        GoogleApiClient.OnConnectionFailedListener,
-//        LocationListener
+import com.example.android.whatsgood.data.WhatsGoodContract.RestaurantEntry;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity //implements LoaderManager.LoaderCallbacks<Cursor>
 {
+    private static final int WHATSGOOD_LOADER = 0;
+
+    //WhatsGoodCursorAdapter mCursorAdapter;
+
+    ArrayList<Restaurant> restaurantsArrayList = new ArrayList<>();
+
+
     /**
      * Spinner drop down arrow to enter day of the week
      */
@@ -49,6 +65,10 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
 
+        //WhatsGoodDbHelper mDbHelper = new WhatsGoodDbHelper(this);
+
+        //SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
@@ -59,28 +79,28 @@ public class MainActivity extends AppCompatActivity
         toolbar.setTitle("");
         toolbar.setSubtitle("");
 
-        // Find the view pager that will allow the user to swipe between fragments
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-
-        // Create an adapter that knows which fragment should be shown on each page
-        TabAdapter adapter = new TabAdapter(this, getSupportFragmentManager());
-
-        // Set the adapter onto the view pager
-        viewPager.setAdapter(adapter);
-
-        // Find the tab layout that shows the tabs
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-
-        // Connect the tab layout with the view pager. This will
-        //   1. Update the tab layout when the view pager is swiped
-        //   2. Update the view pager when a tab is selected
-        //   3. Set the tab layout's tab names with the view pager's adapter's titles
-        //      by calling onPageTitle()
-        tabLayout.setupWithViewPager(viewPager);
-
-        // Use icons for the tabs
-        tabLayout.getTabAt(0).setIcon(R.drawable.ic_list_white);
-        tabLayout.getTabAt(1).setIcon(R.drawable.ic_location_on_white);
+//        // Find the view pager that will allow the user to swipe between fragments
+//        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+//
+//        // Create an adapter that knows which fragment should be shown on each page
+//        TabAdapter adapter = new TabAdapter(this, getSupportFragmentManager());
+//
+//        // Set the adapter onto the view pager
+//        viewPager.setAdapter(adapter);
+//
+//        // Find the tab layout that shows the tabs
+//        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+//
+//        // Connect the tab layout with the view pager. This will
+//        //   1. Update the tab layout when the view pager is swiped
+//        //   2. Update the view pager when a tab is selected
+//        //   3. Set the tab layout's tab names with the view pager's adapter's titles
+//        //      by calling onPageTitle()
+//        tabLayout.setupWithViewPager(viewPager);
+//
+//        // Use icons for the tabs
+//        tabLayout.getTabAt(0).setIcon(R.drawable.ic_list_white);
+//        tabLayout.getTabAt(1).setIcon(R.drawable.ic_location_on_white);
 
         // Get the current day to know which day to display
         //SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
@@ -108,33 +128,33 @@ public class MainActivity extends AppCompatActivity
 
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        // Add a page change listener on the viewPager to show/hide fab depending on which tab is selected
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener()
-        {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels)
-            {
-            }
-
-            @Override
-            public void onPageSelected(int position)
-            {
-                switch (position)
-                {
-                    case 0:
-                        fab.show();
-                        break;
-                    case 1:
-                        fab.hide();
-                        break;
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state)
-            {
-            }
-        });
+//        // Add a page change listener on the viewPager to show/hide fab depending on which tab is selected
+//        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener()
+//        {
+//            @Override
+//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels)
+//            {
+//            }
+//
+//            @Override
+//            public void onPageSelected(int position)
+//            {
+//                switch (position)
+//                {
+//                    case 0:
+//                        fab.show();
+//                        break;
+//                    case 1:
+//                        fab.hide();
+//                        break;
+//                }
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int state)
+//            {
+//            }
+//        });
 
         fab.setOnClickListener(new View.OnClickListener()
         {
@@ -162,6 +182,45 @@ public class MainActivity extends AppCompatActivity
                 builder.show();
             }
         });
+
+        try
+        {
+            restaurantsArrayList = new GetRestaurantsAsyncTask(this).execute().get();
+        } catch (java.lang.InterruptedException e)
+        {
+
+        } catch (java.util.concurrent.ExecutionException e)
+        {
+
+        }
+
+        // Create an {@link RestaurantAdapter}, whose data source is a list of {@link Restaurant}s.
+        // The adapter knows how to create list items for each item in the list.
+        RestaurantAdapter adapter = new RestaurantAdapter(this, restaurantsArrayList, R.color.colorBackground);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // word_list.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
+        // {@link ListView} will display list items for each {@link Word} in the list.
+        listView.setAdapter(adapter);
+
+//        // Find the ListView which will be populated with the restaurants data
+//        ListView restaurantsListView = (ListView) findViewById(R.id.list);
+//
+//        // Find and set the empty view on the ListView, so that it only shows when the list has 0 items
+//        View emptyView = findViewById(R.id.empty_view);
+//        restaurantsListView.setEmptyView(emptyView);
+//
+//        // Setup an Adapter to create a list item for each row of restaurant data in the Cursor.
+//        // There is no restaurant data yet (until the loader finishes) so pass in null for the Cursor.
+//        mCursorAdapter = new WhatsGoodCursorAdapter(this, null);
+//        restaurantsListView.setAdapter(mCursorAdapter);
+//
+//        // Kick off the loader
+//        getLoaderManager().initLoader(WHATSGOOD_LOADER, null, this);
     }
 
     /**
@@ -259,4 +318,47 @@ public class MainActivity extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
+
+//    @Override
+//    public Loader<Cursor> onCreateLoader(int id, Bundle args)
+//    {
+//        // Define a projection that specifies the columns from the table we care about
+//        String[] projection = {
+//                RestaurantEntry._ID,
+//                RestaurantEntry.COLUMN_RESTAURANT_NAME,
+//                RestaurantEntry.COLUMN_RESTAURANT_LINK,
+//                RestaurantEntry.COLUMN_RESTAURANT_ADDRESS,
+//                RestaurantEntry.COLUMN_RESTAURANT_LATITUDE,
+//                RestaurantEntry.COLUMN_RESTAURANT_LONGITUDE,
+//                RestaurantEntry.COLUMN_RESTAURANT_IMAGE_RESOURCE_ID,
+//                RestaurantEntry.COLUMN_RESTAURANT_MONDAY_SPECIALS,
+//                RestaurantEntry.COLUMN_RESTAURANT_TUESDAY_SPECIALS,
+//                RestaurantEntry.COLUMN_RESTAURANT_WEDNESDAY_SPECIALS,
+//                RestaurantEntry.COLUMN_RESTAURANT_THURSDAY_SPECIALS,
+//                RestaurantEntry.COLUMN_RESTAURANT_FRIDAY_SPECIALS,
+//                RestaurantEntry.COLUMN_RESTAURANT_SATURDAY_SPECIALS,
+//                RestaurantEntry.COLUMN_RESTAURANT_SUNDAY_SPECIALS};
+//
+//        // This loader will execute the ContentProvider's query method on a background thread
+//        return new CursorLoader(this,     // Parent activity context
+//                RestaurantEntry.CONTENT_URI,    // Provider content URI to query
+//                projection,
+//                null,
+//                null,
+//                null);
+//    }
+//
+//    @Override
+//    public void onLoadFinished(Loader<Cursor> loader, Cursor data)
+//    {
+//        // Update {@link WhatsGoodCursorAdapter} with this new cursor containing updated pet data
+//        mCursorAdapter.swapCursor(data);
+//    }
+//
+//    @Override
+//    public void onLoaderReset(Loader<Cursor> loader)
+//    {
+//        // Callback called when the data needs to be deleted
+//        mCursorAdapter.swapCursor(null);
+//    }
 }
