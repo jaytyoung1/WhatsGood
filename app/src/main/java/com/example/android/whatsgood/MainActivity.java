c5c7877e1b0aa35d271f71a6a1dd73e9859125cd
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2017-12-30 01:47:14 -0500

    Main Activity again uses TabLayout and ViewPager. Also handles location permission request and location changes.

--- app/src/main/java/com/example/android/whatsgood/MainActivity.java
+++ app/src/main/java/com/example/android/whatsgood/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.android.whatsgood;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
@@ -18,6 +24,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.android.whatsgood.data.GetRestaurantsAsyncTask;
 import com.google.android.gms.common.ConnectionResult;
@@ -34,14 +41,22 @@ public class MainActivity extends AppCompatActivity
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener
 {
+    /**
+     * Database variables
+     */
     //private static final int WHATSGOOD_LOADER = 0;
-
     //WhatsGoodCursorAdapter mCursorAdapter;
 
+    /**
+     * Location variables
+     */
     GoogleApiClient mGoogleApiClient;
     LocationRequest mLocationRequest;
     Location mCurrentLocation;
 
+    /**
+     * ArrayList of restaurants
+     */
     ArrayList<Restaurant> restaurantsArrayList = new ArrayList<>();
 
 
@@ -61,15 +76,29 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
 
-        mGoogleApiClient = new GoogleApiClient.Builder(getApplicationContext())
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-        mGoogleApiClient.connect();
+        //Initialize Google Play Services, ask user for permission
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED)
+            {
+                //Location Permission already granted
+                buildGoogleApiClient();
+                //mGoogleMap.setMyLocationEnabled(true);
+            } else
+            {
+                //Request Location Permission
+                checkLocationPermission();
+            }
+        } else
+        {
+            buildGoogleApiClient();
+            //mGoogleMap.setMyLocationEnabled(true);
+        }
 
+        // Database statements
         //WhatsGoodDbHelper mDbHelper = new WhatsGoodDbHelper(this);
-
         //SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
         // Set the content of the activity to use the activity_main.xml layout file
@@ -82,37 +111,32 @@ public class MainActivity extends AppCompatActivity
         toolbar.setTitle("");
         toolbar.setSubtitle("");
 
-//        // Find the view pager that will allow the user to swipe between fragments
-//        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-//
-//        // Create an adapter that knows which fragment should be shown on each page
-//        TabAdapter adapter = new TabAdapter(this, getSupportFragmentManager());
-//
-//        // Set the adapter onto the view pager
-//        viewPager.setAdapter(adapter);
-//
-//        // Find the tab layout that shows the tabs
-//        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-//
-//        // Connect the tab layout with the view pager. This will
-//        //   1. Update the tab layout when the view pager is swiped
-//        //   2. Update the view pager when a tab is selected
-//        //   3. Set the tab layout's tab names with the view pager's adapter's titles
-//        //      by calling onPageTitle()
-//        tabLayout.setupWithViewPager(viewPager);
-//
-//        // Use icons for the tabs
-//        tabLayout.getTabAt(0).setIcon(R.drawable.ic_list_white);
-//        tabLayout.getTabAt(1).setIcon(R.drawable.ic_location_on_white);
-
-        // Get the current day to know which day to display
-        //SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
-        //Date date = new Date();
-        //String dayOfTheWeek = sdf.format(date);
-
+        // Set up the spinner to select the day of the week
         mDaySpinner = (Spinner) findViewById(R.id.spinner_day_of_the_week);
-
         setupSpinner();
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        TabAdapter adapter = new TabAdapter(this, getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+
+        // Find the tab layout that shows the tabs
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        // Connect the tab layout with the view pager. This will
+        //   1. Update the tab layout when the view pager is swiped
+        //   2. Update the view pager when a tab is selected
+        //   3. Set the tab layout's tab names with the view pager's adapter's titles
+        //      by calling onPageTitle()
+        tabLayout.setupWithViewPager(viewPager);
+
+        // Use icons for the tabs
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_list_white);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_location_on_white);
 
         // Get the map icon and add a click listener to go to the Map activity
         ImageView mapButton = (ImageView) findViewById(R.id.action_button_map);
@@ -176,27 +200,6 @@ public class MainActivity extends AppCompatActivity
 
         }
 
-        // Create an {@link RestaurantAdapter}, whose data source is a list of {@link Restaurant}s.
-        // The adapter knows how to create list items for each item in the list.
-        //RestaurantAdapter adapter = new RestaurantAdapter(this, restaurantsArrayList, R.color.colorBackground);
-        //restAdapter = new RestaurantAdapter(this, restaurantsArrayList, R.color.colorBackground);
-
-        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-        // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // word_list.xml layout file.
-        //ListView listView = (ListView) findViewById(R.id.list);
-
-        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
-        // {@link ListView} will display list items for each {@link Word} in the list.
-        //listView.setAdapter(adapter);
-
-//        // Find the ListView which will be populated with the restaurants data
-//        ListView restaurantsListView = (ListView) findViewById(R.id.list);
-//
-//        // Find and set the empty view on the ListView, so that it only shows when the list has 0 items
-//        View emptyView = findViewById(R.id.empty_view);
-//        restaurantsListView.setEmptyView(emptyView);
-//
 //        // Setup an Adapter to create a list item for each row of restaurant data in the Cursor.
 //        // There is no restaurant data yet (until the loader finishes) so pass in null for the Cursor.
 //        mCursorAdapter = new WhatsGoodCursorAdapter(this, null);
@@ -204,6 +207,19 @@ public class MainActivity extends AppCompatActivity
 //
 //        // Kick off the loader
 //        getLoaderManager().initLoader(WHATSGOOD_LOADER, null, this);
+    }
+
+    /**
+     * Build and connect a Google API client
+     */
+    protected synchronized void buildGoogleApiClient()
+    {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
     }
 
     /**
@@ -249,9 +265,7 @@ public class MainActivity extends AppCompatActivity
                     if (!selection.equals("Pick a day"))
                     {
                         RestaurantAdapter adapter = new RestaurantAdapter(MainActivity.this, restaurantsArrayList, R.color.colorBackground);
-
                         ListView listView = (ListView) findViewById(R.id.list);
-
                         listView.setAdapter(adapter);
                     }
                 }
@@ -291,11 +305,8 @@ public class MainActivity extends AppCompatActivity
         {
             // Sort the ArrayList of restaurants by name
             Collections.sort(restaurantsArrayList, new NameComparator());
-
             RestaurantAdapter adapter = new RestaurantAdapter(this, restaurantsArrayList, R.color.colorBackground);
-
             ListView listView = (ListView) findViewById(R.id.list);
-
             listView.setAdapter(adapter);
 
             return true;
@@ -304,11 +315,8 @@ public class MainActivity extends AppCompatActivity
         {
             // Sort the ArrayList of restaurants by location
             Collections.sort(restaurantsArrayList, new LocationComparator(mCurrentLocation));
-
             RestaurantAdapter adapter = new RestaurantAdapter(this, restaurantsArrayList, R.color.colorBackground);
-
             ListView listView = (ListView) findViewById(R.id.list);
-
             listView.setAdapter(adapter);
 
             return true;
@@ -327,9 +335,7 @@ public class MainActivity extends AppCompatActivity
             }
 
             RestaurantAdapter adapter = new RestaurantAdapter(this, restaurantsArrayList, R.color.colorBackground);
-
             ListView listView = (ListView) findViewById(R.id.list);
-
             listView.setAdapter(adapter);
 
             return true;
@@ -369,13 +375,96 @@ public class MainActivity extends AppCompatActivity
 
         Collections.sort(restaurantsArrayList, new LocationComparator(mCurrentLocation));
 
+        // Create an {@link RestaurantAdapter}, whose data source is a list of {@link Restaurant}s.
+        // The adapter knows how to create list items for each item in the list.
         RestaurantAdapter adapter = new RestaurantAdapter(this, restaurantsArrayList, R.color.colorBackground);
 
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // word_list.xml layout file.
         ListView listView = (ListView) findViewById(R.id.list);
 
+        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
+        // {@link ListView} will display list items for each {@link Word} in the list.
         listView.setAdapter(adapter);
     }
 
+    /**
+     * Methods to handle location permission upon first run
+     */
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    private void checkLocationPermission()
+    {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION))
+            {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Location Permission Needed")
+                        .setMessage("This app needs the Location permission, please accept to use location functionality")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i)
+                            {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+            } else
+            {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        switch (requestCode)
+        {
+            case MY_PERMISSIONS_REQUEST_LOCATION:
+            {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED)
+                    {
+                        if (mGoogleApiClient == null)
+                            buildGoogleApiClient();
+                        //mGoogleMap.setMyLocationEnabled(true);
+                    }
+                } else
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+//    /**
+//     * Database methods
+//     */
 //    @Override
 //    public Loader<Cursor> onCreateLoader(int id, Bundle args)
 //    {
