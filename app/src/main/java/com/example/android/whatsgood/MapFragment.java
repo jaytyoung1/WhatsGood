36aef8ecc82d3c48f2eed1a3e42c79b6efc47cce
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2018-01-20 14:51:58 -0500

    Show to toolbar when going back to the MapFragment

--- app/src/main/java/com/example/android/whatsgood/MapFragment.java
+++ app/src/main/java/com/example/android/whatsgood/MapFragment.java
@@ -13,10 +13,12 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.android.whatsgood.data.GetRestaurantsAsyncTask;
@@ -114,6 +116,14 @@ public class MapFragment extends Fragment
         Context mainContext = inflater.getContext();
         FloatingActionButton fab = ((Activity) mainContext).findViewById(R.id.fab);
         fab.hide();
+
+        // Show the toolbar
+        Toolbar toolbar = ((Activity) mainContext).findViewById(R.id.toolbar);
+        toolbar.setVisibility(View.VISIBLE);
+
+        // Make the listView invisible
+        ListView listView = ((Activity) mainContext).findViewById(R.id.list);
+        listView.setVisibility(View.INVISIBLE);
 
         // If a restaurant was clicked, get it in order to fly to it
         try
