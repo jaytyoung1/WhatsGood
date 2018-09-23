6e9ea8880e247ba0f3e20e2638c25ddbcddbda8e
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2018-09-22 21:00:49 -0400

    Revert "Move adapter classes to separate folder"
    This reverts commit 7ef2e71bc91270b823e228b38fd47f3f9db1e083.

--- app/src/main/java/com/example/android/whatsgood/WhatsGoodCursorAdapter.java
+++ app/src/main/java/com/example/android/whatsgood/WhatsGoodCursorAdapter.java
@@ -1,4 +1,4 @@
-package com.example.android.whatsgood.Adapters;
+package com.example.android.whatsgood;
 
 import android.Manifest;
 import android.content.Context;
@@ -15,8 +15,6 @@ import android.widget.CursorAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.android.whatsgood.MainActivity;
-import com.example.android.whatsgood.R;
 import com.example.android.whatsgood.data.WhatsGoodContract;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
